9c087631a6dd73982229889b507fde549362498b
Luis Eloy Homobono Fuentes <30938644+Luis-Prot@users.noreply.github.com>
2020-06-30 19:06:53 -0500

    Actualización 20200630a
    Actualización
    Fecha:  20200630
    Versión: a

---
+++ map/compbio_groups_mex.js
@@ -0,0 +1,123 @@
+var groups = 
+{"type": "FeatureCollection", "features": [
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3336827,20.7192669]}, "type": "Feature", "properties": 
+{"content":"Agustino Martínez Antonio,CINVESTAV Irapuato, Guanajuato","link":"http://www.ira.cinvestav.mx/Investigaci%C3%B3n/DepartamentodeIngenier%C3%ADaGen%C3%A9tica/ProfesoresInvestigadores/DrMart%C3%ADnezAntonioAgustino/tabid/112/language/es-MX/Default.aspx","title":"Laboratorio de Ingeniería Biológica"}, "id": 1},
+{"geometry": 
+{"type": "Point", "coordinates": [-100.1189881,25.7627999]}, "type": "Feature", "properties": 
+{"content":"Moisés Santillán,CINVESTAV Monterrey, México","link":"http://www.msantillan.org.mx/","title":"Biología Computacional y de Sistemas"}, "id": 2},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1315537,19.5073174]}, "type": "Feature", "properties": 
+{"content":"Juan Carlos Martínez-García,CINVESTAV Zacatenco, CDMX","link":"http://www.conciliencia.org/jcmgNEW/index.php?title=Main_Page","title":"Departamento de Control Automático"}, "id": 3}, 
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1525233,17,19.2864563]}, "type": "Feature", "properties": 
+{"content":"Claudia Rangel Escareño ,INMEGEN, CDMX","link":"http://www.inmegen.gob.mx/es/investigacion/investigadores/curriculum-vitae/?perfil=19","title":"Computational Genomics"}, "id": 4},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1525233,17,19.2864563]}, "type": "Feature", "properties": 
+{"content":"Enrique Hernández Lemus,INMEGEN, CDMX","link":"http://csbig.inmegen.gob.mx/","title":"Computational Systems Biology/Integrative Genomics"}, "id": 5},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1525233,17,19.2864563]}, "type": "Feature", "properties": 
+{"content":"Osbaldo Resendis,INMEGEN, CDMX","link":"http://resendislab.inmegen.gob.mx/","title":"Human Systems Biology Lab"}, "id":6 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1525233,17,19.2864563]}, "type": "Feature", "properties": 
+{"content":"Sandra Romero Hidalgo,INMEGEN, CDMX","link":"-","title":"Genética de Poblaciones"}, "id":7 },
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3336827,20.7192669]}, "type": "Feature", "properties": 
+{"content":"Alexander de Luna,CINVESTAV Irapuato, Guanajuato","link":"http://www.langebio.cinvestav.mx/?pag=118","title":"Genetic Systems Biology"}, "id": 8},
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3336827,20.7192669]}, "type": "Feature", "properties": 
+{"content":"Cei Abreu-Goodger,CINVESTAV Irapuato, Guanajuato","link":"http://ceiabreulab.org/","title":"RNA Genomics Computational Lab"}, "id": 9},
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3336827,20.7192669]}, "type": "Feature", "properties": 
+{"content":"Mauricio Carrillo-Tripp,CINVESTAV Irapuato, Guanajuato","link":"http://www.langebio.cinvestav.mx/?pag=331","title":"Computational Biophysics and Bioinformatics"}, "id": 10},
+{"geometry": 
+{"type": "Point", "coordinates": [-89.6399077,21.029341]}, "type": "Feature", "properties": 
+{"content":"Jorge Humberto Ramírez Prado,Mérida, Yucatán. México","link":"http://www.cicy.mx/unidad-de-biotecnologia/investigador/jorge-humberto-ramirez-prado","title":"Unidad de Biotecnología"}, "id": 11},
+{"geometry":
+{"type": "Point", "coordinates": [-99.2395748,18.9822062]}, "type": "Feature", "properties": 
+{"content":"José Díaz Escudero,UAEM, Cuernavaca, Morelos","link":"http://www.fc.uaem.mx/author/jose-diaz/","title":"Deparmento de Bioquímica y Biología Molecular"}, "id": 12},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1828013,19.3298397]}, "type": "Feature", "properties": 
+{"content":"Ana Escalante Hernández,UNAM, CU. CDMX","link":"http://lancis.ecologia.unam.mx/megadapt/index.php/equipo?p=7","title":"Department of evolutionary ecology"}, "id": 13 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1818304,19.3234472]}, "type": "Feature", "properties": 
+{"content":"Antonio Lazcano,UNAM, CU. CDMX","link":"http://academicos.fciencias.unam.mx/antoniolazcano/","title":"Origen de la Vida, Departamento de Biología Evolutiva"}, "id": 14},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1792888,19.3247793]}, "type": "Feature", "properties": 
+{"content":"Carlos Villareal Luján,UNAM, CU. CDMX","link":"-","title":"Departamento de Física Teórica"}, "id": 15},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1826257,19.325722]}, "type": "Feature", "properties": 
+{"content":"Christopher Rhodes Stephens,UNAM, CU. CDMX","link":"-","title":"Sistemas Complejos"}, "id": 16 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1828013,19.3298397]}, "type": "Feature", "properties": 
+{"content":"David A. Rosenbleuth,UNAM, CU. CDMX","link":"http://turing.iimas.unam.mx/~drosenbl/","title":"Departamento de Ciencias de la Computación"}, "id": 17},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Elena álvarez-Buylla,UNAM, CU. CDMX","link":"http://www.ecologia.unam.mx/invest/ealvarez.html","title":"Ecología Evolutiva. Genética Molecular, Desarrollo y Evolución de Plantas"}, "id": 18 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.181113,19.3280592]}, "type": "Feature", "properties": 
+{"content":"Gabriel Del Río Guerra,UNAM, CU. CDMX","link":"http://www.ifc.unam.mx/investigadores/gabriel-delrio","title":"Bioquímica y Biología Estructural"}, "id": 19},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Luis Mendoza,UNAM, CU. CDMX","link":"http://compbiolab.biomedicas.unam.mx/index.html","title":"Laboratorio de Biología Computacional (CompBioLab)"}, "id": 20},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Mariana Benitez Keinrad,UNAM, CU. CDMX","link":"http://web.ecologia.unam.mx/index.php/theme-features?id=93","title":"Laboratorio Nacional de Ciencias de la Sostenibilidad (LANCIS)"}, "id": 21},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Pablo Padilla,UNAM, CU. CDMX","link":"http://www.fenomec.unam.mx/pablo/","title":"Departamento de Matemáticas y Mecánica"}, "id": 22 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Gustavo Martínez-Mekler,UNAM, CU. CDMX","link":"http://www.fis.unam.mx/~mekler/","title":"Fenómenos no lineales y Complejidad"}, "id": 23},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Ayari Fuentes Hernández,UNAM, Cuernavaca, Morelos","link":"http://fuentes-hernandez.com/","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 24},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Diego Cortez Quezada,UNAM, Cuernavaca, Morelos","link":"http://www.ccg.unam.mx/es/personalInfo?idPersona=891","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 25},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"José Utrilla Carreri,UNAM, Cuernavaca, Morelos","link":"http://www.ccg.unam.mx/es/personalInfo?idPersona=889","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 26},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"María de Lourdes Girard Cuesy,UNAM, Cuernavaca, Morelos","link":"http://www.ccg.unam.mx/es/personalInfo?idPersona=71","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 27 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Rafael Peña Miller,UNAM, Cuernavaca, Morelos","link":"http://www.penamiller.com/","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 28 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Julio Freyre González,UNAM, Cuernavaca, Morelos","link":"-","title":"Regulatory Systems Biology"}, "id": 29 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"María Ávila,Juriquilla, Querétaro","link":"http://liigh.unam.mx/mavila/","title":"Population and Evolutionary Genomics"}, "id": 30},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Guillermo Dávila,Juriquilla, Querétaro","link":"http://liigh.unam.mx/profile/dr-guillermo-davila/","title":"Genome Dynamics and Evolution"}, "id": 31},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Alejandra Medina Rivera,Juriquilla, Querétaro","link":"http://liigh.unam.mx/profile/dra-alejandra-medina-rivera/","title":"Regulatory genomics and bioinformatics"}, "id": 32},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Lucía Morales,Juriquilla, Querétaro","link":"http://liigh.unam.mx/lmorales/","title":"Saccharomycotina yeast genome evolution"}, "id": 33},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Rafael Palacios,Juriquilla, Querétaro","link":"http://liigh.unam.mx/profile/dr-rafael-palacios/","title":"Genome Dynamics, from bacteria to humans"}, "id": 34},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Daniela Robles,Juriquilla, Querétaro","link":"http://liigh.unam.mx/drobles/","title":"Cancer genetics"}, "id": 35},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Enrique Merino,UNAM, Cuernavaca, Morelos","link":"http://www.ibt.unam.mx/biocomputo/","title":"Grupo de Genómica Computacional"}, "id": 36},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Lorenzo Segovia,UNAM, Cuernavaca, Morelos","link":"http://www.ibt.unam.mx/server/PRG.base?tipo:doc,dir:PRG.grupo,par:Gls,tit:_Grupo_del__Dr._Lorenzo_Segovia","title":"Departamento de Ingeniería Celular y Biocatálisis"}, "id": 37},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Enrique Morett,INMEGEN, CDMX","link":"http://www.inmegen.gob.mx/es/investigacion/investigadores/curriculum-vitae/?perfil=2638","title":"Departamento de Genómica Computacional"}, "id": 38},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1946572,19.321551]}, "type": "Feature", "properties": 
+{"content":"Alejandro Sosa Peinado, Facultad de Medicina, UNAM" , "link": "http://laguna.fmedic.unam.mx/~LFQIP/LFQIP/AlejandroS.html", "title": "Dinámica conformacional de Proteínas" }, "id": 39}, 
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1834416,19.3319338]}, "type": "Feature", "properties": 
+{"content":"León Martínez Castilla, Facultad de Química, UNAM" , "link": "http://www.quimica.unam.mx/ficha_investigador.php?ID=44&tipo=2", "title": "Estructura, función y evolución de proteínas" }, "id": 40} 
+]};
---
+++ map/geojson_groups.js
@@ -0,0 +1,135 @@
+﻿var groups = 
+{"type": "FeatureCollection", "features": [
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3323116,20.7200628]}, "type": "Feature", "properties": 
+{"content":"Agustino Martínez Antonio,CINVESTAV Irapuato, Guanajuato","link":"http://www.ira.cinvestav.mx/Investigaci%C3%B3n/DepartamentodeIngenier%C3%ADaGen%C3%A9tica/ProfesoresInvestigadores/DrMart%C3%ADnezAntonioAgustino/tabid/112/language/es-MX/Default.aspx","title":"Laboratorio de Ingeniería Biológica"}, "id": 1},
+{"geometry": 
+{"type": "Point", "coordinates": [-100.1189881,25.7627999]}, "type": "Feature", "properties": 
+{"content":"Moisés Santillán,CINVESTAV Monterrey, México","link":"http://www.msantillan.org.mx/","title":"Biología Computacional y de Sistemas"}, "id": 2},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1315537,19.5073174]}, "type": "Feature", "properties": 
+{"content":"Juan Carlos Martínez-García,CINVESTAV Zacatenco, CDMX","link":"http://www.conciliencia.org/jcmgNEW/index.php?title=Main_Page","title":"Departamento de Control Automático"}, "id": 3}, 
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1499554,19.2861825]}, "type": "Feature", "properties": 
+{"content":"Claudia Rangel Escareño ,INMEGEN, CDMX","link":"http://www.inmegen.gob.mx/es/investigacion/investigadores/curriculum-vitae/?perfil=19","title":"Computational Genomics"}, "id": 4},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1499554,19.2861825]}, "type": "Feature", "properties": 
+{"content":"Enrique Hernández Lemus,INMEGEN, CDMX","link":"http://csbig.inmegen.gob.mx/","title":"Computational Systems Biology/Integrative Genomics"}, "id": 5},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1499554,19.2861825]}, "type": "Feature", "properties": 
+{"content":"Osbaldo Resendis,INMEGEN, CDMX","link":"http://resendislab.inmegen.gob.mx/","title":"Human Systems Biology Lab"}, "id":6 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1499554,19.2861825]}, "type": "Feature", "properties": 
+{"content":"Sandra Romero Hidalgo,INMEGEN, CDMX <br> (Group website not available)","link":"#","title":"Genética de Poblaciones"}, "id":7 },
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3323116,20.7200628]}, "type": "Feature", "properties": 
+{"content":"Alexander de Luna, LANGEBIO, Irapuato, Guanajuato","link":"http://www.langebio.cinvestav.mx/?pag=118","title":"Genetic Systems Biology"}, "id": 8},
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3323116,20.7200628]}, "type": "Feature", "properties": 
+{"content":"Cei Abreu-Goodger, LANGEBIO, Irapuato, Guanajuato","link":"http://ceiabreulab.org/","title":"RNA Genomics Computational Lab"}, "id": 9},
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3323116,20.7200628]}, "type": "Feature", "properties": 
+{"content":"Mauricio Carrillo-Tripp, LANGEBIO, Irapuato, Guanajuato","link":"http://www.langebio.cinvestav.mx/?pag=331","title":"Computational Biophysics and Bioinformatics"}, "id": 10},
+{"geometry": 
+{"type": "Point", "coordinates": [-89.6399077,21.029341]}, "type": "Feature", "properties": 
+{"content":"Jorge Humberto Ramírez Prado,Mérida, Yucatán. México","link":"http://www.cicy.mx/unidad-de-biotecnologia/investigador/jorge-humberto-ramirez-prado","title":"Unidad de Biotecnología"}, "id": 11},
+{"geometry":
+{"type": "Point", "coordinates": [-99.2395748,18.9822062]}, "type": "Feature", "properties": 
+{"content":"José Díaz Escudero,UAEM, Cuernavaca, Morelos","link":"http://www.fc.uaem.mx/author/jose-diaz/","title":"Deparmento de Bioquímica y Biología Molecular"}, "id": 12},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1828013,19.3298397]}, "type": "Feature", "properties": 
+{"content":"Ana Escalante Hernández,UNAM, CU. CDMX","link":"http://lancis.ecologia.unam.mx/megadapt/index.php/equipo?p=7","title":"Department of evolutionary ecology"}, "id": 13 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1796418,19.3234469]}, "type": "Feature", "properties": 
+{"content":"Antonio Lazcano,UNAM, CU. CDMX","link":"http://academicos.fciencias.unam.mx/antoniolazcano/","title":"Origen de la Vida, Departamento de Biología Evolutiva"}, "id": 14},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1771001,19.3247793]}, "type": "Feature", "properties": 
+{"content":"Carlos Villareal Luján,UNAM, CU. CDMX","link":"http://www.fisica.unam.mx/ifunam_english/teorica/faculty.php","title":"Departamento de Física Teórica"}, "id": 15},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1826257,19.325722]}, "type": "Feature", "properties": 
+{"content":"Christopher Rhodes Stephens,UNAM, CU. CDMX <br> (Group website not available)","link":"#","title":"Sistemas Complejos"}, "id": 16 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1828013,19.3298397]}, "type": "Feature", "properties": 
+{"content":"David A. Rosenbleuth,UNAM, CU. CDMX","link":"http://turing.iimas.unam.mx/~drosenbl/","title":"Departamento de Ciencias de la Computación"}, "id": 17},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1924687,19.3215511]}, "type": "Feature", "properties": 
+{"content":"Elena álvarez-Buylla,UNAM, CU. CDMX","link":"http://www.ecologia.unam.mx/invest/ealvarez.html","title":"Ecología Evolutiva. Genética Molecular, Desarrollo y Evolución de Plantas"}, "id": 18 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1789243,19.3280592]}, "type": "Feature", "properties": 
+{"content":"Gabriel Del Río Guerra,UNAM, CU. CDMX","link":"http://www.ifc.unam.mx/investigadores/gabriel-delrio","title":"Bioquímica y Biología Estructural"}, "id": 19},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1920771,19.3230609]}, "type": "Feature", "properties": 
+{"content":"Luis Mendoza,UNAM, CU. CDMX","link":"http://compbiolab.biomedicas.unam.mx/index.html","title":"Laboratorio de Biología Computacional (CompBioLab)"}, "id": 20},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.191941,19.3225794]}, "type": "Feature", "properties": 
+{"content":"Mariana Benitez Keinrad,UNAM, CU. CDMX","link":"http://web.ecologia.unam.mx/index.php/personal?id=93","title":"Laboratorio Nacional de Ciencias de la Sostenibilidad (LANCIS)"}, "id": 21},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1828013,19.3298397]}, "type": "Feature", "properties": 
+{"content":"Pablo Padilla,UNAM, CU. CDMX","link":"http://www.fenomec.unam.mx/pablo/","title":"Departamento de Matemáticas y Mecánica"}, "id": 22 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2368359,18.9825632]}, "type": "Feature", "properties": 
+{"content":"Gustavo Martínez-Mekler,UNAM, CU. CDMX","link":"http://www.fis.unam.mx/~mekler/","title":"Fenómenos no lineales y Complejidad"}, "id": 23},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2374849,18.9831856]}, "type": "Feature", "properties": 
+{"content":"Ayari Fuentes Hernández,UNAM, Cuernavaca, Morelos","link":"http://fuentes-hernandez.com/","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 24},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2374849,18.9831856]}, "type": "Feature", "properties": 
+{"content":"Diego Cortez Quezada,UNAM, Cuernavaca, Morelos","link":"http://www.ccg.unam.mx/es/personalInfo?idPersona=891","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 25},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2374849,18.9831856]}, "type": "Feature", "properties": 
+{"content":"José Utrilla Carreri,UNAM, Cuernavaca, Morelos","link":"http://www.ccg.unam.mx/es/personalInfo?idPersona=889","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 26},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2374849,18.9831856]}, "type": "Feature", "properties": 
+{"content":"María de Lourdes Girard Cuesy,UNAM, Cuernavaca, Morelos","link":"http://www.ccg.unam.mx/es/personalInfo?idPersona=71","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 27 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2374849,18.9831856]}, "type": "Feature", "properties": 
+{"content":"Rafael Peña Miller,UNAM, Cuernavaca, Morelos","link":"http://www.penamiller.com/","title":"Laboratorio de Biología de Sistemas y Biología Sintética"}, "id": 28 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2374849,18.9831856]}, "type": "Feature", "properties": 
+{"content":"Julio Freyre González,UNAM, Cuernavaca, Morelos","link":"https://www.researchgate.net/profile/Julio_Augusto_Freyre-Gonzalez","title":"Regulatory Systems Biology"}, "id": 29 },
+{"geometry": 
+{"type": "Point", "coordinates": [-100.4472131,20.7030015]}, "type": "Feature", "properties": 
+{"content":"María Ávila,Juriquilla, Querétaro","link":"http://liigh.unam.mx/mavila/","title":"Population and Evolutionary Genomics"}, "id": 30},
+{"geometry": 
+{"type": "Point", "coordinates": [-100.4472131,20.7030015]}, "type": "Feature", "properties": 
+{"content":"Guillermo Dávila,Juriquilla, Querétaro","link":"http://liigh.unam.mx/profile/dr-guillermo-davila/","title":"Genome Dynamics and Evolution"}, "id": 31},
+{"geometry": 
+{"type": "Point", "coordinates": [-100.4472131,20.7030015]}, "type": "Feature", "properties": 
+{"content":"Alejandra Medina Rivera,Juriquilla, Querétaro","link":"http://liigh.unam.mx/profile/dra-alejandra-medina-rivera/","title":"Regulatory genomics and bioinformatics"}, "id": 32},
+{"geometry": 
+{"type": "Point", "coordinates": [-100.4472131,20.7030015]}, "type": "Feature", "properties": 
+{"content":"Lucía Morales,Juriquilla, Querétaro","link":"http://liigh.unam.mx/lmorales/","title":"Saccharomycotina yeast genome evolution"}, "id": 33},
+{"geometry": 
+{"type": "Point", "coordinates": [-100.4472131,20.7030015]}, "type": "Feature", "properties": 
+{"content":"Rafael Palacios,Juriquilla, Querétaro","link":"http://liigh.unam.mx/profile/dr-rafael-palacios/","title":"Genome Dynamics, from bacteria to humans"}, "id": 34},
+{"geometry": 
+{"type": "Point", "coordinates": [-100.4472131,20.7030015]}, "type": "Feature", "properties": 
+{"content":"Daniela Robles,Juriquilla, Querétaro","link":"http://liigh.unam.mx/drobles/","title":"Cancer genetics"}, "id": 35},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2363181,18.9838957]}, "type": "Feature", "properties": 
+{"content":"Enrique Merino,UNAM, Cuernavaca, Morelos","link":"http://www.ibt.unam.mx/biocomputo/","title":"Grupo de Genómica Computacional"}, "id": 36 },
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2363181,18.9838957]}, "type": "Feature", "properties": 
+{"content":"Lorenzo Segovia,UNAM, Cuernavaca, Morelos","link":"http://www.ibt.unam.mx/server/PRG.base?tipo:doc,dir:PRG.grupo,par:Gls,tit:_Grupo_del__Dr._Lorenzo_Segovia","title":"Departamento de Ingeniería Celular y Biocatálisis"}, "id": 37},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1499554,19.2861825]}, "type": "Feature", "properties": 
+{"content":"Enrique Morett,INMEGEN, CDMX","link":"http://www.inmegen.gob.mx/es/investigacion/investigadores/curriculum-vitae/?perfil=2638","title":"Departamento de Genómica Computacional"}, "id": 38},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.1774487,19.329234]}, "type": "Feature", "properties": 
+{"content":"Alejandro Sosa Peinado, Facultad de Medicina, UNAM" , "link": "http://laguna.fmedic.unam.mx/~LFQIP/LFQIP/AlejandroS.html", "title": "Dinámica conformacional de Proteínas" }, "id": 39}, 
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3323116,20.7200628]}, "type": "Feature", "properties": 
+{"content":"Francisco Barona Gómez, LANGEBIO, Irapuato, Guanajuato" , "link": "http://www.langebio.cinvestav.mx/?pag=120", "title": "Evolution of Metabolic Diversity" }, "id": 40}, 
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2374849,18.9831856]}, "type": "Feature", "properties": 
+{"content":"Julio Collado Vides, UNAM, Cuernavaca, Morelos" , "link": "http://www.ccg.unam.mx/en/personalInfo?idPersona=39", "title": "Computational Genomics" }, "id": 41}, 
+{"geometry": 
+{"type": "Point", "coordinates": [-99.2374849,18.9831856]}, "type": "Feature", "properties": 
+{"content":"Pablo Vinuesa, UNAM, Cuernavaca, Morelos" , "link": "http://www.ccg.unam.mx/~vinuesa/", "title": "Environmental and Evolutionary Microbiology" }, "id": 42},
+{"geometry": 
+{"type": "Point", "coordinates": [-99.180168,19.3311093]}, "type": "Feature", "properties": 
+{"content":"León Martínez Castilla, Facultad de Química, UNAM" , "link": "http://www.quimica.unam.mx/ficha_investigador.php?ID=44&tipo=2", "title": "Estructura, función y evolución de proteínas" }, "id": 43},
+{"geometry": 
+{"type": "Point", "coordinates": [-101.3323116,20.7200628]}, "type": "Feature", "properties": 
+{"content":"Selene Fernández Valverde, LANGEBIO, Irapuato, Guanajuato" , "link": "http://www.langebio.cinvestav.mx/?pag=850", "title": "Regulatory RNA Lab" }, "id": 44}
+]};
